89319ac3e2a2d6080b16cae64a1bfc9d8d87af01
TahreemZafar <105108420+TahreemZafar@users.noreply.github.com>
2023-06-12 20:05:25 +0500

    initial commit

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,9 +1,10 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function loading() {
   return (
     <div className='flex justify-center mt-10'>
-       <img src="spin1.svg" alt="loading..." />
+       <Image src="spin1.svg" alt="loading..." />
     </div>
   )
 }
