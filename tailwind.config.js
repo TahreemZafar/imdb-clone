167c000c48f1cbdf81759fa43b37c2d142e0cb84
TahreemZafar <105108420+TahreemZafar@users.noreply.github.com>
2023-10-08 09:09:56 +0500

    changes in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  purge: [ "./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}", ],
+
   theme: {
     extend: {
       backgroundImage: {
